92c995b3f80c48899c7f835d3e3ac90ecc5e7a9d
hadyd <anr.hadyd@gmail.com>
2020-04-27 18:41:50 +0700

    First Init

---
+++ luasLingkaran.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	nilaiJariJari := 7.0
+	vhi := 3.14
+
+	luasLingkaran := vhi * (nilaiJariJari * nilaiJariJari)
+
+	fmt.Println("Luas lingkaran dengan jari-jari", nilaiJariJari, "cm adalah", luasLingkaran, "cm")
+}
